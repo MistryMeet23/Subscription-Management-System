e1031854ef5a990dc71985135e3e274165318123
Dhruvin Jariwala <dhruvinjariwala422002@gmail.com>
2024-12-09 10:21:11 +0530

    9-12

--- SubMaster Backend/Subscription Management System/obj/Debug/net8.0/Subscription Management System.AssemblyInfo.cs
+++ SubMaster Backend/Subscription Management System/obj/Debug/net8.0/Subscription Management System.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Subscription Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8021342fa42ffa98f1782e95cf722fadf2853c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13578a4f193c64cbaa389be799e088291b22b6fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Subscription Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Subscription Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
