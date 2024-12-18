57d55190803fcb1b5f7e15cd76744e174ded5f2f
Meet Mistry <mistrymeet6338@gmail.com>
2024-12-18 21:40:49 +0530

    testing

--- SubMaster Backend/Subscription Management System/obj/Debug/net8.0/Subscription Management System.AssemblyInfo.cs
+++ SubMaster Backend/Subscription Management System/obj/Debug/net8.0/Subscription Management System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Subscription Management System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23ff140608c5ea25f6123b9d6927824aeeb14d27")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cda52d91ec43653fb6b58e0d4b98dd6e94b1d8d8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Subscription Management System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Subscription Management System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
